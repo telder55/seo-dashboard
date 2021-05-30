5e8c3b3eb665522c58dfb4d72fb69eb8f5d890d1
TJ Elder <telder55@gmail.com>
2021-05-30 13:59:28 -0600

    Added button to homepage

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BackgroundSmall from "../../images/background-small.jpg";
 import Backgroundlarge from "../../images/background-large.jpg";
+import Button from "@material-ui/core/Button";
 import "./style.css";
 
 const small = BackgroundSmall;
@@ -13,13 +14,16 @@ class Home extends React.Component {
         <h1>
           Grow your traffic organically
           <br />
-          with SEO Dashboard
+          with SEO Dashboard <br />
+          <Button variant="contained" color="secondary">
+            Get Started
+          </Button>
         </h1>
+
         <img
           src={small}
           srcSet={`${small} 300w, ${small} 768w, ${large} 1280w,`}
           className="background-image"
-          onLoad={this.onLoad}
         />
       </div>
     );
