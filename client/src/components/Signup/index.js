d08d4ac66c5f5129310403b146043bd9e3a9de5b
TJ Elder <telder55@gmail.com>
2021-06-02 21:29:03 -0600

    Signup form is posting to database

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -1,9 +1,9 @@
 // Take 'disabled' attribute off of Button when form can be submitted.
-
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import API from "../../utils/API";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,29 +16,77 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignupForm() {
   const classes = useStyles();
+  const [formObject, setFormObject] = useState({});
+
+  useEffect(() => {}, []);
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormObject({ ...formObject, [name]: value });
+    console.log(formObject);
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    if (formObject.first && formObject.last) {
+      API.saveUser({
+        first: formObject.first,
+        last: formObject.last,
+        email: formObject.email,
+        password: formObject.password,
+      })
+        .then((res) => console.log("IDK"))
+        .catch((err) => console.log(err));
+    }
+  }
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
       <div>
-        <TextField required id="standard-required" label="First Name" /> <br />
-        <TextField required id="standard-required" label="Last Name" /> <br />
         <TextField
           required
-          id="standard-password-input"
+          name="first"
+          label="First Name"
+          onChange={handleInputChange}
+        />{" "}
+        <br />
+        <TextField
+          required
+          name="last"
+          label="Last Name"
+          onChange={handleInputChange}
+        />{" "}
+        <br />
+        <TextField
+          required
+          name="email"
+          label="Email Address"
+          onChange={handleInputChange}
+        />{" "}
+        <br />
+        <TextField
+          required
+          name="password"
           label="Password"
+          onChange={handleInputChange}
           type="password"
           autoComplete="current-password"
         />{" "}
         <br />
         <TextField
           required
-          id="standard-password-input"
+          name="confirm"
           label="Confirm Password"
+          onChange={handleInputChange}
           type="password"
           autoComplete="current-password"
         />
       </div>
-      <Button variant="contained" disabled>
+      <Button
+        variant="contained"
+        disabled={!(formObject.first && formObject.last)}
+        onClick={handleFormSubmit}
+      >
         Create Account
       </Button>
     </form>
