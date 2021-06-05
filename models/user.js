b5f570b852b6ec4861cb183bb66c8759a9699c77
TJ Elder <telder55@gmail.com>
2021-06-05 10:21:45 -0600

    Added email to user model

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   first: { type: String, required: true },
   last: { type: String, required: true },
+  email: { type: String, required: true },
   password: { type: String, required: true },
   date: { type: Date, default: Date.now },
 });
