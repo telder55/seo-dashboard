900679985be865f26748c43e8df636ee50876a5f
TJ Elder <telder55@gmail.com>
2021-06-07 19:33:41 -0600

    Password is being hashed when seeding user db

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -13,22 +13,39 @@ const userSeed = [
     password: "password",
     date: new Date(Date.now()),
   },
-  {
-    first: "Melanie",
-    last: "Elder",
-    email: "mel@gmail.com",
-    password: "password",
-    date: new Date(Date.now()),
-  },
+  // {
+  //   first: "Melanie",
+  //   last: "Elder",
+  //   email: "mel@gmail.com",
+  //   password: "password",
+  //   date: new Date(Date.now()),
+  // },
 ];
 
 db.User.remove({})
-  .then(() => db.User.collection.insertMany(userSeed))
-  .then((data) => {
-    console.log(data.result.n + " records inserted!");
-    process.exit(0);
+  .then(() =>
+    db.User.create({
+      first: "TJ",
+      last: "Elder",
+      email: "telder55@gmail.com",
+      password: "abcd@1234",
+    })
+  )
+  .then((res) => {
+    console.log("res", res);
   })
   .catch((err) => {
     console.error(err);
     process.exit(1);
   });
+
+// db.User.remove({})
+//   .then(() => db.User.collection.insertMany(userSeed))
+//   .then((data) => {
+//     console.log(data.result.n + " records inserted!");
+//     process.exit(0);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     process.exit(1);
+//   })
