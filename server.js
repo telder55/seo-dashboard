b66f5510e66e7423750f5c5423e5a9fc00df8c47
TJ Elder <telder55@gmail.com>
2021-06-11 16:41:11 -0600

    Added code to generate URL for google auth in server.js

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const { google } = require("googleapis");
+const oauth2 = google.oauth2("v2");
+
+//Google Api stuff
+
+const oauth2Client = new google.auth.OAuth2(
+  "581749580855-7os7thh02ips6t6okk4hdklg0s54eopu.apps.googleusercontent.com",
+  "YUBY58RHR7TKKDCKllgO66ZQ",
+  "http://localhost:3000/dashboard"
+);
+
+const scopes = "https://www.googleapis.com/auth/webmasters";
+
+const url = oauth2Client.generateAuthUrl({
+  // 'online' (default) or 'offline' (gets refresh_token)
+  access_type: "offline",
+
+  // If you only need one scope you can pass it as a string
+  scope: scopes,
+});
+
+console.log(url);
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
