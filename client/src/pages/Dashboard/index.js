5b00a11cb917899558ce1b435eeaf59438d19f9f
TJ Elder <telder55@gmail.com>
2021-06-12 09:36:41 -0600

    Removed url from dashboard

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -10,7 +10,7 @@ const Dashboard = () => {
   return (
     <div>
       <h1>Welcome to Your Dashboard!</h1>
-      <a href="https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fwebmasters&response_type=code&client_id=581749580855-7os7thh02ips6t6okk4hdklg0s54eopu.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fdashboard">
+      <a href="">
         <Button>
           Click this very button to let us look at your search data please.{" "}
         </Button>
